2307e626c7ae9fb0f17cc822872d385d3ff944e3
Abdurrasheed <abdurrasheedfalaludaura@gmail.com>
2023-04-26 14:03:44 +0100

    fix some changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+    document.querySelector('.cart  span').classList.toggle('window-scroll-cart', window.scrollY > 0)
 
 })
